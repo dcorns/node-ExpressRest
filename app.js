6c0af43ee86b9e48bd76ee41732003771e3a6ace
Dale Corns <drcorns@hotmail.com>
2015-05-20 00:04:20 -0700

    Very basic node express API

---
+++ app.js
@@ -0,0 +1,38 @@
+/**
+ * app.js
+ * Created by dcorns on 5/19/15.
+ * Where the server begins
+ */
+'use strict';
+var express = require('express');
+var app = express();
+
+// respond with "Hello World!" on the homepage
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+// accept POST request on the homepage
+app.post('/', function (req, res) {
+  res.send('Got a POST request');
+});
+
+// accept PUT request at /user
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user');
+});
+
+// accept DELETE request at /user
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user');
+});
+
+
+//console.log(process.env);
+
+var server = app.listen(process.env.PORT || 3000, function(){
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
